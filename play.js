c45a6227580b27d54521cd7b7839be9487536c7f
Amitava Das Gupta <amitavadg@gmail.com>
2019-01-08 10:00:04 +0530

    Destructuring

--- play.js
+++ play.js
@@ -7,23 +7,37 @@ const person = {
   }
 };
 
-const hobbies = ["Sports", "Cooking"];
-// for (let hobby of hobbies) {
-//   console.log(hobby);
-// }
-// console.log(hobbies.map(hobby => "Hobby: " + hobby));
-// console.log(hobbies);
-// hobbies.push('Programming');
-// console.log(hobbies);
-const copiedArray = [...hobbies];
-// Spread operator [...] puls out the elements of a nested array
-// and puts them in the top level array.
-console.log(copiedArray);
-
-// the rest operator [...] followed by argument in a function alows any number
-// of arguments when caling the funstion.
-const toArray = (...args) => {
-  return args;
+// Destructuring objects
+const printName = ({ name }) => {
+  console.log(name);
 };
 
-console.log(toArray(1, 2, 3, 4));
+printName(person);
+
+const { name, age } = person;
+console.log(name, age);
+
+// Destructuring arrays
+const hobbies = ["Sports", "Cooking"];
+const [hobby1, hobby2] = hobbies;
+console.log(hobby1, hobby2);
+
+// // for (let hobby of hobbies) {
+// //   console.log(hobby);
+// // }
+// // console.log(hobbies.map(hobby => "Hobby: " + hobby));
+// // console.log(hobbies);
+// // hobbies.push('Programming');
+// // console.log(hobbies);
+// // Spread operator [...] puls out the elements of a nested array
+// // and puts them in the top level array.
+// const copiedArray = [...hobbies];
+// console.log(copiedArray);
+
+// // the rest operator [...] followed by argument in a function alows any number
+// // of arguments when caling the funstion.
+// const toArray = (...args) => {
+//   return args;
+// };
+
+// console.log(toArray(1, 2, 3, 4));
