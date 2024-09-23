e88bdd2330236332cec9c4e626b4e24315e38253
Amitava Dasgupta <amitaigl.@gmail.com>
2024-09-23 11:20:17 +0530

    Syntax corrections

--- play.js
+++ play.js
@@ -1,19 +1,7 @@
-const name = 'Amitava';
-let age = 52;
-const hasHobbies = true;
-
-const summariseUser(userName, userAge, userhasHobby) {
-    return(
-       'Name is ' +
-       userName +
-       ', age is ' +
-       userAge +
-       ' and the user has hobbies' 
-    );
-}
-
-const add=(a,b) => {
-    return a + b;
-}
-console.log(summariseUser(name, age, hasHobbies))
-console.log(add(2, 4));
+const person = {
+    name: 'Amitava',
+    age: 72,
+    greet: => {
+        console.log(person);
+    },
+};
